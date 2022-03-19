448430ec20572293f4df93250b02fd119cb7e14c
mgouriprasad <65456223+mgouriprasad@users.noreply.github.com>
2022-03-20 02:26:11 +0530

    index

--- myApp.js
+++ myApp.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 /*console.log("Hello World");
-app.get('/', (req, res) => { res.send("Hello Express") });*/
-var absolutePath = __dirname+views/index.html
-app.get('/', (req, res) => { res.sendFile(absolutePath) });
+app.get('/', (req, res) => { res.send("Hello Express") });
+var absolutePath = __dirname+views/index.html*/
+app.get('/', (req, res) => { res.sendFile(/views/index.html) });
 
 
 
