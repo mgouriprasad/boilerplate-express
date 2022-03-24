c1fd292cf287f9ea9867a083cccf79759ed8da67
mgouriprasad <65456223+mgouriprasad@users.noreply.github.com>
2022-03-25 04:22:35 +0530

    jsonupper1

--- myApp.js
+++ myApp.js
@@ -1,11 +1,16 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 /*console.log("Hello World");
 app.get('/', (req, res) => { res.send("Hello Express") });*/
 app.get('/', (req, res) => { res.sendFile(__dirname+"/views/index.html") });
 app.use("/public", express.static(__dirname+"/public"));
-app.get('/json', (req, res) => {res.json({"message":"Hello json"})});
-
+var l_hello = "Hello json";
+if (process.env.MESSAGE_STYLE="uppercase")
+{
+    l_hello = l_hello.toUpperCase
+}
+app.get('/json', (req, res) => {res.json({"message":l_hello})});
 
 
 
