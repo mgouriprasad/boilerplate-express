5f37691783b699c89ea55685e4dca747df8dc90f
mgouriprasad <65456223+mgouriprasad@users.noreply.github.com>
2022-03-25 03:54:23 +0530

    API

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ var app = express();
 app.get('/', (req, res) => { res.send("Hello Express") });*/
 app.get('/', (req, res) => { res.sendFile(__dirname+"/views/index.html") });
 app.use("/public", express.static(__dirname+"/public"));
-
+app.get('/json', (req, res) => {res.json({"message":"Hello json"})});
 
 
 
