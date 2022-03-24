c2b9900bfe38d6af033777675a8ff8b8017db277
mgouriprasad <65456223+mgouriprasad@users.noreply.github.com>
2022-03-25 02:50:29 +0530

    12*

--- myApp.js
+++ myApp.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 /*console.log("Hello World");
-app.get('/', (req, res) => { res.send("Hello Express") });*/
-app.get('/', (req, res) => { res.sendFile(__dirname+"/views/index.html") });
+app.get('/', (req, res) => { res.send("Hello Express") });
+app.get('/', (req, res) => { res.sendFile(__dirname+"/views/index.html") });*/
 app.use(__dirname+"/public", express.static());
 
 
