f098e6af3b9a45da9869217950e4dccf01182983
mgouriprasad <65456223+mgouriprasad@users.noreply.github.com>
2022-04-30 02:34:34 +0530

    a

--- myApp.js
+++ myApp.js
@@ -5,14 +5,14 @@ var app = express();
 app.get('/', (req, res) => { res.send("Hello Express") });*/
 app.get('/', (req, res) => { res.sendFile(__dirname+"/views/index.html") });
 app.use("/public", express.static(__dirname+"/public"));
-var l_hello = "Hello json";
+var l_hello = "hello json";
 console.log(process.env.MESSAGE_STYLE)
 if (process.env.MESSAGE_STYLE=="uppercase")
 {
     l_hello=l_hello.toUpperCase();
     console.log(l_hello);
 } 
-console.log({"message":'l_hello'});
+console.log({"message":"l_hello"});
 app.get('/json', (req, res) => {res.json({ message :l_hello})});
 
 
