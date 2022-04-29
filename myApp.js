7ead1a62697ec812ab5ec122b1357d7fc016b8e7
mgouriprasad <65456223+mgouriprasad@users.noreply.github.com>
2022-04-30 02:53:02 +0530

    a

--- myApp.js
+++ myApp.js
@@ -2,19 +2,18 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 /*console.log("Hello World");
-app.get('/', (req, res) => { res.send("Hello Express") });*/
-app.get('/', (req, res) => { res.sendFile(__dirname+"/views/index.html") });
+app.get('/', (req, res) => { res.send("Hello Express") });
+app.get('/', (req, res) => { res.sendFile(__dirname+"/views/index.html") });*/
 app.use("/public", express.static(__dirname+"/public"));
-var l_hello = "hello json";
-console.log(process.env.MESSAGE_STYLE)
-if (process.env.MESSAGE_STYLE=="uppercase")
+var l_init = "hello json";
+console.log(process.env.MESSAGE_STYLE);
+if (process.env.MESSAGE_STYLE === "uppercase")
 {
-    l_hello=l_hello.toUpperCase();
-    console.log(l_hello);
-} 
-console.log({"message":"l_hello"});
-app.get('/json', (req, res) => {res.json({ message :l_hello})});
-
+    l_hello = l_init.toUpperCase();
+} else {
+    l_hello = l_init;
+}
+app.get('/', (req, res) => { res.json({message:l_message})});
 
 
 
