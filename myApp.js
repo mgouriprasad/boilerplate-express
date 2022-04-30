d6e9a31d3f19834b9da70eab48364d424a09bc08
mgouriprasad <65456223+mgouriprasad@users.noreply.github.com>
2022-04-30 12:17:53 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -5,16 +5,14 @@ var app = express();
 app.get('/', (req, res) => { res.send("Hello Express") });*/
 app.get('/', (req, res) => { res.sendFile(__dirname+"/views/index.html") });
 app.use("/public", express.static(__dirname+"/public"));
-var l_init = "Hello json";
-console.log(process.env.MESSAGE_STYLE);
 
-console.log({message:l_message});
 app.get('/json', (req, res) => { 
     if (process.env.MESSAGE_STYLE === "uppercase")
     {
         res.json({message:"HELLO JSON"});
-    } 
-    res.json({message:"Hello json"})
+    } else {
+        res.json({message:"Hello json"});
+    }
                                 });
 
 
