7199f573ec6419d57e4dc0feb343ee782aeab400
mgouriprasad <65456223+mgouriprasad@users.noreply.github.com>
2022-04-30 12:14:04 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -13,9 +13,8 @@ app.get('/json', (req, res) => {
     if (process.env.MESSAGE_STYLE === "uppercase")
     {
         res.json({message:"HELLO JSON"});
-    } else {
+    } 
     res.json({message:"Hello json"});
-            }
                                 });
 
 
